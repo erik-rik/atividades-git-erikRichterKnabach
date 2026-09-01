b7ff6997cb38103ea13b3d97e0ce89652be1370e
Erik Richter Knabach <erik.knabach@gmail.com>
2026-08-31 21:27:54 -0300

    feat: cria projeto AtividadesGit com a classe Principal

---
+++ AtividadesGit/src/atividades/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividades;
+
+/**
+ *
+ * @author erikk
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Aluno: Seu Nome Completo");
+        System.out.println("Curso: Programacao Java");
+        System.out.println("Atividade de Git e GitHub");
+    }
+    
+}
