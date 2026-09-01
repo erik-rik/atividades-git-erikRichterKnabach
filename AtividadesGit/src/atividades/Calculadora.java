2e4ea351911270fa55bcba4f5b76e38fae997625
Erik Richter Knabach <erik.knabach@gmail.com>
2026-08-31 21:33:14 -0300

    fix: evita divisao por zero no metodo media

--- AtividadesGit/src/atividades/Calculadora.java
+++ AtividadesGit/src/atividades/Calculadora.java
@@ -15,6 +15,9 @@ public class Calculadora {
     }
 
     public double media(double[] valores) {
+        if (valores == null || valores.length == 0) {
+            return 0;
+        }
         double soma = 0;
         for (double v : valores) {
             soma = soma + v;
